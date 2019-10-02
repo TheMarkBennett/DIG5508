bd4e48831aa1eef9802ce68b2738825325d1b1a7
Mark Bennett <mark.bennett@ucf.edu>
2019-10-02 13:16:00 -0400

    add add numbers

---
+++ lab1-week3/test.py
@@ -0,0 +1,15 @@
+#%%
+def addNumbers(value1,value2):
+    return value1 + value2
+
+#%%
+def subtractNumbers(value1,value2):
+    return value1 - value2
+
+#%%
+var1 = 1
+var2 = 2
+var3 = 3
+addNumbers(12,5)
+
+#%%
