803bcc0af4cab97abcbe58349d7ba5c53a683ada
Mark Bennett <mark.bennett@ucf.edu>
2019-10-16 21:45:57 -0400

    new excercise

--- exercises/10-6-19-concept_7-4.py
+++ exercises/10-6-19-concept_7-4.py
@@ -1,4 +1,11 @@
 
-def sortdata(input):
-    if(input)
-    return 1
+##This files detemines if a number is even or odd
+#%%
+def sentence(value):
+    if 'football' in value:
+        return "Working"
+    else:
+        return "Not working"
+    
+
+#%%
