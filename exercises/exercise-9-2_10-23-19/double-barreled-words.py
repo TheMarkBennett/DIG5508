68d65301442a35d9d9caae97e5fe3b08a44b55c6
Mark Bennett <mark.bennett@ucf.edu>
2019-10-23 18:14:29 -0400

    week9

---
+++ exercises/exercise-9-2_10-23-19/double-barreled-words.py
@@ -0,0 +1,31 @@
+# Open function to open the file "leaves-of-grass.txt"
+#%%
+##from gutenberg.acquire import load_etext
+##from gutenberg.cleanup import strip_headers
+##from textblob import TextBlob
+
+##text = strip_headers(load_etext(1322)).strip()
+##blob = TextBlob(text)
+# print(text)  # prints 'MOBY DICK; OR THE WHALE\n\nBy Herman Melville ...' exercises\exercise-9-2_10-23-19\double-barreled-words.py
+# This will save the text to a local .txt file in this directory.
+
+##source = open('exercises/exercise-9-2_10-23-19/leavesofgrass.txt','w',encoding="utf-16",newline='\n')
+##source.write(text)
+##source.close()
+
+
+#%%
+
+source1  = open("exercises/exercise-9-2_10-23-19/prideandprejudice.txt","r") 
+pride = source1.read()
+
+source2  = open("exercises/exercise-9-2_10-23-19/leavesofgrass.txt","r") 
+leave = source2.read()
+
+
+import re
+
+result = re.findall(r'[\w]+-[\w]+', leave)
+len(result)
+
+#%%
